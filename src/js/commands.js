9682669ae90577baf6201e957d311f8708435314
Liam Hockley <xdega121@gmail.com>
2018-10-05 10:51:52 -0500

    Update syntax format; Nuke excessive comments

--- src/js/commands.js
+++ src/js/commands.js
@@ -1,70 +1,58 @@
-var commands = ["grab", "move", "set", "use", "combine", "look", "examine", "prepare"];
+var commands = [
+    "grab", 
+    "move", 
+    "set", 
+    "use", 
+    "combine", 
+    "look", 
+    "examine", 
+    "prepare"
+];
 
 $("#commands").keypress(function(e) {
-
-	//only do something when they press enter
 	if(e.which == 13) {
-		//cache the commands text input
 		var elm = $(this);
-
-		//grab the value from the text input
 		var command = elm.val();
-
-		//make all words lowercase and split the string into an array
 		var command_array = command.toLowerCase().split(" ");
 
-		//did they type in a valid command
-		if ( $.inArray(command_array[0], commands) !== -1 ){
-
-			//which command did they issue?
+		if($.inArray(command_array[0], commands) !== -1 ) {
 			switch (command_array[0]) {
 				case "grab":
 					grab(command_array);
 					break;
 
-				//done
 				case "move":
 					move(command_array);
 					break;
 
-				//BROCK - done!
 				case "set":
 					set(command_array);
 					break;
 
-				//WILL
 				case "use":
 					use(command_array);
 					break;
 
-				//PHASE 2
 				case "combine":
 					combine(command_array);
 					break;
 
-				//RYAN - done!
 				case "look":
 					look(command_array);
 					break;
 
-				//BRAD
 				case "examine":
 					examine(command_array);
 					break;
 
-                //BROCK
 				case "prepare":
 					prepare(command_array);
 					break;
-
 			}
 
-		}else{
-			//didn't give me a valid command
-			result("You're so scared, you aren't giving valid commands!");
+		} else {
+            result("You're so scared, you aren't giving valid commands!");
 		}
-
-		//empty the #command box
 		elm.val("");
 	}
 });
